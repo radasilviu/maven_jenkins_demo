a1776f6d01b622f1c99429403e836a2783f5a0ed
Silviu Rada <silviu.rada@gmail.com>
2020-10-23 12:41:19 +0300

    add project

---
+++ src/main/java/ThemeParkRide.java
@@ -0,0 +1,68 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class ThemeParkRide {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private int thrillFactor;
+    private int vomitFactor;
+
+    public ThemeParkRide() {
+
+    }
+
+    public ThemeParkRide(String name, String description, int thrillFactor, int vomitFactor) {
+        this.name = name;
+        this.description = description;
+        this.thrillFactor = thrillFactor;
+        this.vomitFactor = vomitFactor;
+    }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public int getThrillFactor() {
+    return thrillFactor;
+  }
+
+  public void setThrillFactor(int thrillFactor) {
+    this.thrillFactor = thrillFactor;
+  }
+
+  public int getVomitFactor() {
+    return vomitFactor;
+  }
+
+  public void setVomitFactor(int vomitFactor) {
+    this.vomitFactor = vomitFactor;
+  }
+}
